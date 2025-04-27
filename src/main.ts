01d87b76e51a7e5ae471cbaf89d0b02a12778d22
Felipe Evaldt Carvalho <felipeevaldt1@gmail.com>
2025-04-27 00:17:17 -0300

    config pacotes primeu, primeicons e vue3-mq

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,9 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router.ts";
 import PrimeVue, { primevueConfig } from "./plugins/primevue";
+import { Vue3Mq } from "vue3-mq";
 import "./style.css";
+import "primeicons/primeicons.css";
 
 const app = createApp(App);
 app.use(router);
@@ -13,5 +15,9 @@ app.use(PrimeVue, {
       darkModeSelector: false || "none",
     },
   },
+});
+app.use(Vue3Mq, {
+  global: true,
+  preset: "tailwind",
 });
 app.mount("#app");
