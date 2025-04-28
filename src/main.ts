5cd04dc33eb293dca2dd3a1b651d88042cda34c4
Felipe Evaldt Carvalho <felipeevaldt1@gmail.com>
2025-04-28 17:56:55 -0300

    build: remove lib vue3-mq

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,6 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router.ts";
 import PrimeVue, { primevueConfig } from "./plugins/primevue";
-import { Vue3Mq } from "vue3-mq";
 import "./style.css";
 import "primeicons/primeicons.css";
 
@@ -15,9 +14,5 @@ app.use(PrimeVue, {
       darkModeSelector: false || "none",
     },
   },
-});
-app.use(Vue3Mq, {
-  global: true,
-  preset: "tailwind",
 });
 app.mount("#app");
