e0433f47a3fee263edfdbe1421e80260b3b01fc2
Felipe Evaldt Carvalho <felipeevaldt1@gmail.com>
2025-08-04 23:48:52 -0300

    feat: atualiza estilos do botão e do componente WFloat para consistência visual

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -3,7 +3,7 @@
   <a
     href="https://wa.me/5551984561140"
     target="_blank"
-    class="w-full lg:!w-fit flex items-center justify-center gap-2 rounded-lg bg-green-700 px-4 py-2 text-sm font-semibold text-white transition-colors hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-offset-2"
+    class="w-full lg:!w-fit flex items-center justify-center gap-2 rounded-lg bg-[#445937] px-4 py-2 text-sm font-semibold text-white transition-colors hover:bg-[#546e43]"
   >
     <i class="pi pi-whatsapp !text-white"></i>
     <span class="!text-white">Entre em contato</span>
