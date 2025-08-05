ee3a93deb08e78fefe45b0f687ef0147d4a7cdac
Felipe Evaldt Carvalho <felipeevaldt1@gmail.com>
2025-08-05 00:55:28 -0300

    feat: adiciona borda e sombra ao botão do WhatsApp para melhor destaque visual

--- src/components/WFloat.vue
+++ src/components/WFloat.vue
@@ -3,7 +3,7 @@
   <a
     href="https://wa.me/5551984561140"
     target="_blank"
-    class="fixed bottom-3 right-3 bg-[#445937] h-11 w-11 md:h-16 md:w-16 rounded-full aspect-square flex items-center justify-center z-50 hover:scale-110 transition-transform"
+    class="fixed bottom-3 right-3 bg-[#445937] h-11 w-11 md:h-16 md:w-16 rounded-full aspect-square flex items-center justify-center z-50 hover:scale-110 transition-transform border-1 border-white shadow-lg shadow-white/20"
   >
     <i class="pi pi-whatsapp !text-white !text-2xl md:!text-4xl"></i>
   </a>
