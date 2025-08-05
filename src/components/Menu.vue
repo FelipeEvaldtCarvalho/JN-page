1ff3ac9b94003b6e5de21acbfcb4798e2fecb9d6
Felipe Evaldt Carvalho <felipeevaldt1@gmail.com>
2025-08-04 23:49:15 -0300

    feat: melhora estilos do menu para responsividade e consistência visual

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -13,24 +13,36 @@ const mq = useMq();
 const visible = ref(false);
 </script>
 <template>
-  <div class="flex w-full fixed top-0 left-0 bg-[#FFD4C1] z-30 text-[#32302F]">
+  <div
+    class="flex w-full fixed top-0 left-0 z-30 text-[#32302F] bg-[#FFD4C1]/30 backdrop-blur-md border border-white/30 shadow-md shadow-white/20"
+  >
     <nav
       class="p-4 flex justify-between items-center w-full max-w-[1400px] mx-auto"
     >
       <img src="/logo.svg" alt="Logo Jaqueline da Nova" />
-      <i class="pi pi-bars !text-3xl lg:!hidden" @click="visible = true"></i>
       <ul class="hidden lg:flex gap-6 items-center text-lg">
         <template v-for="({ label, href }, index) in links" :key="index">
           <li>
-            <a :href="href" class="relative group inline-block">
+            <a
+              :href="href"
+              class="relative group inline-block !text-[#445937] text-base font-semibold"
+            >
               {{ label }}
               <span
-                class="absolute left-0 bottom-0 w-full h-0.5 bg-[#32302F] scale-x-0 group-hover:scale-x-100 transition-transform duration-300 origin-left"
+                class="absolute left-0 -bottom-1 w-full h-0.5 bg-[#445937] scale-x-0 group-hover:scale-x-100 transition-transform duration-300 origin-left"
               ></span>
             </a>
           </li>
         </template>
       </ul>
+      <a
+        href="https://wa.me/5551984561140"
+        target="_blank"
+        class="w-fit flex items-center justify-center gap-2 rounded-lg bg-[#445937] px-4 py-2 text-sm font-semibold text-white transition-colors hover:bg-[#546e43]"
+      >
+        <span class="!text-white">Agendar</span>
+      </a>
+      <i class="pi pi-bars !text-3xl lg:!hidden" @click="visible = true"></i>
     </nav>
   </div>
   <Drawer
