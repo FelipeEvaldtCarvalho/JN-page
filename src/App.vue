16743e0c98a004d091f57f4ea964fc690dfa3707
Felipe Evaldt Carvalho <felipeevaldt1@gmail.com>
2025-08-25 21:23:28 -0300

    feat: simplifica App.vue para ser apenas um container de rotas

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,9 @@
 <script setup>
-import Menu from "./components/Menu.vue";
-import TopBanner from "./components/TopBanner.vue";
-import Text from "./components/Text.vue";
-import Video from "./components/Video.vue";
-import About from "./components/About.vue";
-import TrgBenefits from "./components/TrgBenefits.vue";
-import Testimonials from "./components/Testimonials.vue";
-import Footer from "./components/Footer.vue";
-import WFloat from "./components/WFloat.vue";
-
-import { aboutTrg, whyTrgWorks, sessionInfo, final } from "./texts/texts.js";
+// App.vue agora é apenas o container principal para as rotas
 </script>
 
 <template>
-  <Menu />
-  <TopBanner />
-  <Text v-bind="aboutTrg" id="trg" />
-  <Video />
-  <About />
-  <Text v-bind="whyTrgWorks" color id="porque-funciona" />
-  <TrgBenefits />
-  <Text v-bind="sessionInfo" />
-  <Testimonials />
-  <Text v-bind="final" />
-  <Footer />
-  <WFloat />
+  <router-view />
 </template>
 <style>
 * {
